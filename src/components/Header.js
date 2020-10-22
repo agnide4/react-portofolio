200b86d7cbb4532539ed8efc3f929e88ab7d841d
Samir Bello <agnide4@gmail.com>
2020-10-22 18:15:57 -0500

    redux files created//conditional redering up and running

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Avatar from '@material-ui/core/Avatar';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,12 +30,15 @@ export default function Header() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <Hidden smUp>
+                <MenuIcon  />
+          </Hidden>
+           
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Discover me
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
         </Toolbar>
       </AppBar>
     </div>
