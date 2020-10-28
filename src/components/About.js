45e081482055976627697e1f6ff929914b6a3f3a
Samir Bello <agnide4@gmail.com>
2020-10-28 00:08:01 -0500

    About page all set

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Grid from '@material-ui/core/Grid';
+import {Paper} from "@material-ui/core"
 
 
 
@@ -11,7 +12,41 @@ export default function About() {
 
     return (
         <div>
-            <h1>This is about me</h1>
+            <Grid container spacing={3}>
+                <Grid item sm={6}><Paper xs={12} >
+                    
+                    <h3>
+                        <p>
+                        I'm Samir and I would like to welcome to my page.
+                        I am a junior full stack developper with an enthusiatic attitude towards new challenges.
+                        I am looking forward to joining a team where knowledge is nurtured and
+                        talent appreciated. 
+                        </p>
+                        <p>
+                        Constantly learning new things keeps me alive and I am hoping to take on the challenge
+                        of finding inovative solutions for the mainly challenges mankind faces. 
+                        </p>
+                        <p>
+                        As developers we have a lot to offer the world............ 
+                        </p>
+                        
+                    </h3>
+                
+                </Paper></Grid>
+                <Grid item sm={6}><h1>Technologies I use</h1>
+                    <Paper>
+                    <p>Throughout my learning journey I have acquainted myself with many Technologies like
+                     <h5>Javascript, HTML, Java, C++</h5>
+                      When it comes to databases I can comfortably work in <h5>SQL(MySQL) and noSQL(MongoDb)</h5> databases and would love to learn new ones as well
+                     Other technologies I use daily are <h5>CSS, JQuery, Node JS, React, redux, bootStrap, Material UI, GIT and github, express JS</h5>
+                     I am also well versed in cloud technoligies(AWS) and continue to enrich my mind with technologies
+                     while practicing the one I`ve already learned
+                    </p>
+                    </Paper>
+                    
+                </Grid>
+            </Grid>
+            
         </div>
     )
 
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {useSelector} from "react-redux"
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Header from '../components/Header';
 import Vtab from '../components/Vtab';
+import About from "../components/About"
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +15,14 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 export default function Layout() {
+
+    const [tabValue, error] = useSelector((gState) => [
+        gState.tabValue,
+        gState.error
+    ])
+
     const classes = useStyles();
+    
 
 
 
@@ -27,7 +36,15 @@ export default function Layout() {
                 </Grid>
                 <Grid item container>
                         <Grid item sm={2}> <Vtab /></Grid>
-                        <Grid item xs={12} sm={9}></Grid>
+                        <Grid item xs={12} sm={9}>
+                            {tabValue === 0 ? (
+                                <About />
+                            ) : tabValue === 1 ? (
+                                <h1>Portofolio</h1>
+                            ) : (
+                                <h3>Am good</h3>
+                            )}
+                        </Grid>
                 </Grid>
                 
             </Grid>
