ed7a47289de3a2fe80d18bf65b7de08b3e6c0637
Samir Bello <agnide4@gmail.com>
2020-12-06 00:49:43 -0600

    Updates

---
+++ precache-manifest.1cdff77c87791b1378e1bfd23fe6dab3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cca91dfa858010bbf0a3bafd61c4d5",
+    "url": "/react-portofolio/index.html"
+  },
+  {
+    "revision": "b9d86189a8a61ffdf69c",
+    "url": "/react-portofolio/static/css/main.9a72c1e7.chunk.css"
+  },
+  {
+    "revision": "bb00540b0dd34e7fd5fa",
+    "url": "/react-portofolio/static/js/2.d8f7b8e4.chunk.js"
+  },
+  {
+    "revision": "714aecc39840c1fe5324ab729126fa35",
+    "url": "/react-portofolio/static/js/2.d8f7b8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d86189a8a61ffdf69c",
+    "url": "/react-portofolio/static/js/main.009078ac.chunk.js"
+  },
+  {
+    "revision": "069aa235ffcee3b25455",
+    "url": "/react-portofolio/static/js/runtime-main.b3959288.js"
+  },
+  {
+    "revision": "2c1c1d16aca1cc80d5d2d8c70833d4cf",
+    "url": "/react-portofolio/static/media/Coding Boot Camp Certificate Program.2c1c1d16.pdf"
+  },
+  {
+    "revision": "1471039f031711af3921537e7b81c243",
+    "url": "/react-portofolio/static/media/DayPlanner.1471039f.JPG"
+  },
+  {
+    "revision": "16f5fc14906ef133a526ac2b52af66c0",
+    "url": "/react-portofolio/static/media/Eat-Daburger.16f5fc14.JPG"
+  },
+  {
+    "revision": "a4b3e47cfe7c2a77f223b16f47e82057",
+    "url": "/react-portofolio/static/media/EmployeeD.a4b3e47c.JPG"
+  },
+  {
+    "revision": "2365a3f43306538453bb0f35201bd9b0",
+    "url": "/react-portofolio/static/media/NoteTaker.2365a3f4.JPG"
+  },
+  {
+    "revision": "4e0aedc1f2e64a93ca7242ee727e55c7",
+    "url": "/react-portofolio/static/media/Ppicture.4e0aedc1.jpg"
+  },
+  {
+    "revision": "009376757b8a44e079786370bc660f04",
+    "url": "/react-portofolio/static/media/PricingSupport.00937675.pdf"
+  },
+  {
+    "revision": "b71f31b789068d8b6de8a3797442984c",
+    "url": "/react-portofolio/static/media/Quiz.b71f31b7.JPG"
+  },
+  {
+    "revision": "5167b8fe3d21df0dd03b58953223419e",
+    "url": "/react-portofolio/static/media/Richland College.5167b8fe.pdf"
+  },
+  {
+    "revision": "c1e211cf137f000398ecf8010d1c5561",
+    "url": "/react-portofolio/static/media/Springboard project.c1e211cf.JPG"
+  },
+  {
+    "revision": "bdfcd7fec01f4f34af62b73379c0e6c0",
+    "url": "/react-portofolio/static/media/architecture.bdfcd7fe.pdf"
+  },
+  {
+    "revision": "25529b6b562db8baaa029a02a217a029",
+    "url": "/react-portofolio/static/media/cloudSecurity.25529b6b.pdf"
+  },
+  {
+    "revision": "0a2eed15d71ef52913ecc01bedb0a927",
+    "url": "/react-portofolio/static/media/fitnessTracker.0a2eed15.JPG"
+  },
+  {
+    "revision": "285043cd351bf958a377ea08742d8478",
+    "url": "/react-portofolio/static/media/gBooks.285043cd.JPG"
+  },
+  {
+    "revision": "f3907146333ffc2bb6a5229e80af92e4",
+    "url": "/react-portofolio/static/media/practitioner.f3907146.pdf"
+  },
+  {
+    "revision": "84859e69edb7509b9e9894f10befdba6",
+    "url": "/react-portofolio/static/media/pwdGenerator.84859e69.JPG"
+  },
+  {
+    "revision": "45447c3f653a58286652c4254ab0fa97",
+    "url": "/react-portofolio/static/media/weatherboard.45447c3f.JPG"
+  }
+]);
